aaa53b7979d36d92c2fc133ae72df09fca6ac059
MARTIN_WANG <martinwang7963@gmail.com>
2024-01-12 17:28:53 +0800

    first commit

---
+++ Controllers/BotController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace TelegramBotAPIDemo.Controllers
+{
+	[Route("[controller]/[action]")]
+	[ApiController]
+	public class BotController : ControllerBase
+	{
+
+		[HttpPost]
+		public async Task<IActionResult> HandleRequest([FromBody] Update update)
+		{
+			return Ok();
+		}
+	}
+}
